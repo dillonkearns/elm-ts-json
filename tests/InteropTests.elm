7a68cf6bad2a62f14792fb9e6675932152e2aa41
Dillon Kearns <dillon@dillonkearns.com>
2020-11-04 15:12:13 -0800

    Add encoder test case.

--- tests/InteropTests.elm
+++ tests/InteropTests.elm
@@ -2,42 +2,74 @@ module InteropTests exposing (..)
 
 import Expect exposing (Expectation)
 import Json.Decode
+import Json.Encode as Encode
 import Test exposing (..)
 
 
 suite : Test
 suite =
     describe "Interop"
-        [ test "string" <|
-            \() ->
-                string
-                    |> expectInterop
-                        { input = "\"Hello!\""
-                        , output = "Hello!"
-                        , typeDef = "string"
-                        }
-        , test "list string" <|
-            \() ->
-                list string
-                    |> expectInterop
-                        { input = "[\"Item 1\", \"Item 2\"]"
-                        , output = [ "Item 1", "Item 2" ]
-                        , typeDef = "string[]"
-                        }
+        [ describe "decode"
+            [ test "string" <|
+                \() ->
+                    string
+                        |> expectDecodes
+                            { input = "\"Hello!\""
+                            , output = "Hello!"
+                            , typeDef = "string"
+                            }
+            , test "list string" <|
+                \() ->
+                    list string
+                        |> expectDecodes
+                            { input = "[\"Item 1\", \"Item 2\"]"
+                            , output = [ "Item 1", "Item 2" ]
+                            , typeDef = "string[]"
+                            }
+            ]
+        , describe "encode"
+            [ test "list string" <|
+                \() ->
+                    list string
+                        |> expectEncodes
+                            { input = [ "Item 1", "Item 2" ]
+                            , output = "[\"Item 1\",\"Item 2\"]"
+                            , typeDef = "string[]"
+                            }
+            ]
         ]
 
 
-expectInterop :
+expectDecodes :
     { input : String, output : value, typeDef : String }
     -> Interop value
     -> Expect.Expectation
-expectInterop expect interop =
+expectDecodes expect interop =
     expect.input
         |> Json.Decode.decodeString (decoder interop)
         |> Expect.all
             [ \decoded -> decoded |> Expect.equal (Ok expect.output)
             , \decoded -> expect.typeDef |> Expect.equal (typeDef interop)
             ]
+
+
+expectEncodes :
+    { output : String, input : value, typeDef : String }
+    -> Interop value
+    -> Expect.Expectation
+expectEncodes expect interop =
+    expect.input
+        |> encoder interop
+        |> Encode.encode 0
+        |> Expect.all
+            [ \encodedString -> encodedString |> Expect.equal expect.output
+            , \decoded -> expect.typeDef |> Expect.equal (typeDef interop)
+            ]
+
+
+encoder : Interop value -> (value -> Encode.Value)
+encoder interop =
+    \_ -> Encode.list Encode.string [ "Item 1", "Item 2" ]
 
 
 typeDef (Interop jsonDecoder typeDef_) =
