2bce03d4aa10400eb559433dc98968be8f82dff7
Dillon Kearns <dillon@dillonkearns.com>
2020-11-15 18:07:42 -0800

    Add doc comments in decode module.

--- src/TsInterop/Decode.elm
+++ src/TsInterop/Decode.elm
@@ -1,20 +1,64 @@
-module TsInterop.Decode exposing (..)
+module TsInterop.Decode exposing
+    ( InteropDecoder
+    , bool, float, int, string
+    , list, nullable, oneOf
+    , map
+    , literal
+    , decoder, tsTypeToString
+    )
+
+{-|
+
+
+## Decoders
+
+@docs InteropDecoder
+
+
+## Built-Ins
+
+@docs bool, float, int, string
+
+
+## Composite Types
+
+@docs list, nullable, oneOf
+
+
+## Transformations
+
+@docs map
+
+
+## TypeScript Literals
+
+@docs literal
+
+
+## Using Decoders
+
+@docs decoder, tsTypeToString
+
+-}
 
 import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
 import TsType exposing (TsType(..))
 
 
+{-| -}
 map : (value -> mapped) -> InteropDecoder value -> InteropDecoder mapped
 map mapFn (InteropDecoder innerDecoder innerType) =
     InteropDecoder (Decode.map mapFn innerDecoder) innerType
 
 
+{-| -}
 nullable : InteropDecoder value -> InteropDecoder (Maybe value)
 nullable (InteropDecoder innerDecoder innerType) =
     InteropDecoder (Decode.nullable innerDecoder) (Union [ innerType, TsType.null ])
 
 
+{-| -}
 oneOf : List (InteropDecoder value) -> InteropDecoder value
 oneOf decoders =
     InteropDecoder
@@ -34,10 +78,13 @@ oneOf decoders =
         )
 
 
+{-| -}
 type InteropDecoder value
     = InteropDecoder (Decoder value) TsType
 
 
+{-| TypeScript has support for literals.
+-}
 literal : value -> Encode.Value -> InteropDecoder value
 literal value literalValue =
     InteropDecoder
@@ -54,36 +101,43 @@ literal value literalValue =
         (Literal literalValue)
 
 
+{-| -}
 string : InteropDecoder String
 string =
     InteropDecoder Decode.string String
 
 
+{-| -}
 int : InteropDecoder Int
 int =
     InteropDecoder Decode.int TsType.Number
 
 
+{-| -}
 float : InteropDecoder Float
 float =
     InteropDecoder Decode.float TsType.Number
 
 
+{-| -}
 bool : InteropDecoder Bool
 bool =
     InteropDecoder Decode.bool TsType.Boolean
 
 
+{-| -}
 list : InteropDecoder value -> InteropDecoder (List value)
 list (InteropDecoder innerDecoder innerType) =
     InteropDecoder (Decode.list innerDecoder) (List innerType)
 
 
+{-| -}
 decoder : InteropDecoder value -> Decoder value
 decoder (InteropDecoder decoder_ tsType_) =
     decoder_
 
 
+{-| -}
 tsTypeToString : InteropDecoder value -> String
 tsTypeToString (InteropDecoder decoder_ tsType_) =
     TsType.tsTypeToString_ tsType_
