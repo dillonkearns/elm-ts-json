8c1039ae6ec8c7c5b0060beec3f6f2bb144f9196
Dillon Kearns <dillon@dillonkearns.com>
2021-01-24 19:12:33 -0800

    Use helper function to separate top-level JSON values in json schema from nested objects.

--- src/TsType.elm
+++ src/TsType.elm
@@ -339,27 +339,29 @@ parenthesizeToString type_ =
 
 toJsonSchema : TsType -> Encode.Value
 toJsonSchema tsType =
+    toJsonSchemaHelp tsType
+        |> Encode.object
+
+
+toJsonSchemaHelp : TsType -> List ( String, Encode.Value )
+toJsonSchemaHelp tsType =
     case tsType of
         String ->
-            Encode.object
-                [ ( "type", Encode.string "string" )
-                ]
+            [ ( "type", Encode.string "string" )
+            ]
 
         Number ->
-            Encode.object
-                [ ( "type", Encode.string "number" )
-                ]
+            [ ( "type", Encode.string "number" )
+            ]
 
         Integer ->
-            Encode.object
-                [ ( "type", Encode.string "integer" )
-                ]
+            [ ( "type", Encode.string "integer" )
+            ]
 
         List listType ->
-            Encode.object
-                [ ( "type", Encode.string "array" )
-                , ( "items", toJsonSchema listType )
-                ]
+            [ ( "type", Encode.string "array" )
+            , ( "items", toJsonSchema listType )
+            ]
 
         TypeObject properties ->
             let
@@ -380,81 +382,73 @@ toJsonSchema tsType =
                             )
                         |> List.NonEmpty.fromList
             in
-            Encode.object
-                ([ Just ( "type", Encode.string "object" )
-                 , Just
-                    ( "properties"
-                    , Encode.object
-                        (properties
-                            |> List.map
-                                (\( requiredProperty, name, propertyType ) ->
-                                    ( name, toJsonSchema propertyType )
-                                )
-                        )
+            [ Just ( "type", Encode.string "object" )
+            , Just
+                ( "properties"
+                , Encode.object
+                    (properties
+                        |> List.map
+                            (\( requiredProperty, name, propertyType ) ->
+                                ( name, toJsonSchema propertyType )
+                            )
                     )
-                 , requiredProperties
-                    |> Maybe.map List.NonEmpty.toList
-                    |> Maybe.map
-                        (\requiredProps -> ( "required", Encode.list Encode.string requiredProps ))
-                 ]
-                    |> List.filterMap identity
                 )
+            , requiredProperties
+                |> Maybe.map List.NonEmpty.toList
+                |> Maybe.map
+                    (\requiredProps -> ( "required", Encode.list Encode.string requiredProps ))
+            ]
+                |> List.filterMap identity
 
         Boolean ->
-            Encode.object
-                [ ( "type", Encode.string "boolean" )
-                ]
+            [ ( "type", Encode.string "boolean" )
+            ]
 
         Unknown ->
-            Encode.object []
+            []
 
         Literal literalJson ->
-            Encode.object [ ( "const", literalJson ) ]
+            [ ( "const", literalJson ) ]
 
         Union nonEmptyTypes ->
-            Encode.object
-                [ ( "anyOf"
-                  , nonEmptyTypes
-                        |> List.NonEmpty.toList
-                        |> Encode.list toJsonSchema
-                  )
-                ]
+            [ ( "anyOf"
+              , nonEmptyTypes
+                    |> List.NonEmpty.toList
+                    |> Encode.list toJsonSchema
+              )
+            ]
 
         Tuple tupleTypes maybeRestType ->
             case maybeRestType of
                 Just restType ->
-                    Encode.object
-                        [ ( "additionalItems", toJsonSchema restType )
-                        , ( "items"
-                          , Encode.list toJsonSchema tupleTypes
-                          )
-                        , ( "minItems", Encode.int (List.length tupleTypes) )
-                        , ( "type", Encode.string "array" )
-                        ]
+                    [ ( "additionalItems", toJsonSchema restType )
+                    , ( "items"
+                      , Encode.list toJsonSchema tupleTypes
+                      )
+                    , ( "minItems", Encode.int (List.length tupleTypes) )
+                    , ( "type", Encode.string "array" )
+                    ]
 
                 Nothing ->
-                    Encode.object
-                        [ ( "items"
-                          , Encode.list toJsonSchema tupleTypes
-                          )
-                        , ( "maxItems", Encode.int (List.length tupleTypes) )
-                        , ( "minItems", Encode.int (List.length tupleTypes) )
-                        , ( "type", Encode.string "array" )
-                        ]
+                    [ ( "items"
+                      , Encode.list toJsonSchema tupleTypes
+                      )
+                    , ( "maxItems", Encode.int (List.length tupleTypes) )
+                    , ( "minItems", Encode.int (List.length tupleTypes) )
+                    , ( "type", Encode.string "array" )
+                    ]
 
         Intersection intersectionTypes ->
-            Encode.object
-                [ ( "allOf"
-                  , intersectionTypes
-                        |> Encode.list toJsonSchema
-                  )
-                ]
+            [ ( "allOf"
+              , intersectionTypes
+                    |> Encode.list toJsonSchema
+              )
+            ]
 
         ObjectWithUniformValues objectValueType ->
-            Encode.object
-                [ ( "additionalProperties", toJsonSchema objectValueType )
-                , ( "type", Encode.string "object" )
-                ]
+            [ ( "additionalProperties", toJsonSchema objectValueType )
+            , ( "type", Encode.string "object" )
+            ]
 
         ArrayIndex first rest ->
             let
@@ -468,34 +462,32 @@ toJsonSchema tsType =
                         |> List.maximum
                         |> Maybe.withDefault 0
             in
-            Encode.object
-                [ ( "additionalItems", toJsonSchema Unknown )
-                , ( "items"
-                  , (List.range 0 highestIndex
-                        |> List.map
-                            (\cur ->
-                                Dict.get cur dict
-                                    |> Maybe.withDefault Unknown
-                            )
-                    )
-                        |> Encode.list toJsonSchema
-                  )
-                , ( "minItems", Encode.int (highestIndex + 1) )
-                , ( "type", Encode.string "array" )
-                ]
+            [ ( "additionalItems", toJsonSchema Unknown )
+            , ( "items"
+              , (List.range 0 highestIndex
+                    |> List.map
+                        (\cur ->
+                            Dict.get cur dict
+                                |> Maybe.withDefault Unknown
+                        )
+                )
+                    |> Encode.list toJsonSchema
+              )
+            , ( "minItems", Encode.int (highestIndex + 1) )
+            , ( "type", Encode.string "array" )
+            ]
 
         TsNever ->
-            Encode.object
-                [ {- There may be a more direct way to
-                     express this. This is just a roundabout
-                     way of saying this can never happen.
-                     Because a JSON value will never be both
-                     a `string` and `boolean`, this is a
-                     contradiction, and therefore equivalent
-                     to marking a JSON value with a
-                     TypeScript `never` type.
-                  -}
-                  ( "allOf"
-                  , Encode.list toJsonSchema [ String, Boolean ]
-                  )
-                ]
+            [ {- There may be a more direct way to
+                 express this. This is just a roundabout
+                 way of saying this can never happen.
+                 Because a JSON value will never be both
+                 a `string` and `boolean`, this is a
+                 contradiction, and therefore equivalent
+                 to marking a JSON value with a
+                 TypeScript `never` type.
+              -}
+              ( "allOf"
+              , Encode.list toJsonSchema [ String, Boolean ]
+              )
+            ]
