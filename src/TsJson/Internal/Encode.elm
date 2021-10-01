68972fc062ed46814733a36354a5399643e9e05e
Dillon Kearns <dillon@dillonkearns.com>
2021-10-01 08:45:28 -0700

    Delete unused.

--- src/TsJson/Internal/Encode.elm
+++ src/TsJson/Internal/Encode.elm
@@ -1,14 +1,8 @@
-module TsJson.Internal.Encode exposing (Encoder(..), intersectTypes)
+module TsJson.Internal.Encode exposing (Encoder(..))
 
 import Internal.TsJsonType exposing (TsType)
-import Internal.TypeReducer as TypeReducer
 import Json.Encode as Encode
 
 
 type Encoder input
     = Encoder (input -> Encode.Value) TsType
-
-
-intersectTypes : Encoder input -> TsType -> Encoder input
-intersectTypes (Encoder encoder_ tsType1) tsType2 =
-    Encoder encoder_ (TypeReducer.intersect tsType1 tsType2)
